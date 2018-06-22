e264bfb52a5f92e387d584d4de5e2410fb32e046
chuonghuynh <huynhduychuong@gmail.com>
2018-06-22 14:28:52 +0700

    Add RPCProvider for remote deployment

--- truffle.js
+++ truffle.js
@@ -1,3 +1,5 @@
+var RpcProvider = require('./wallet-provider/RpcProvider.js');
+const admin_key = 'a23bab9997d45e730d5a72d35918d3d53efad56c64d53455edfe7420a3577fe5';
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -13,6 +15,27 @@ module.exports = {
       useColors: true
     },
 
+    rinkeby: {
+      provider: function() {
+        return new RpcProvider(admin_key, "https://rinkeby.infura.io")
+      },
+      gasPrice:3000000000,
+      network_id: 4
+    },
+    etz: {
+      provider: function() {
+        return new RpcProvider(admin_key, "https://rpc.etherzero.org")
+      },
+      gasPrice:30000000000,
+      network_id: 88
+    },
+    clo: {
+      provider: function() {
+        return new RpcProvider(admin_key, "http://127.0.0.1:8545")
+      },
+      gasPrice:30000000000,
+      network_id: 4
+    },
     ganache: {
       host: "127.0.0.1",
       port: 8545,
