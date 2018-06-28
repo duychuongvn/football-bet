7933c537fa1e68d23bf49c2d390a2f695420b15e
Dang Chi Son <kenshin@Shinigami-2.local>
2018-06-28 14:43:46 +0700

    Remove code

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -1,11 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { HomeModule } from 'app/home/home.module';
-import { AdminModule } from 'app/admin/admin.module';
-import { UserModule } from 'app/user/user.module';
-import { MatchDetailModule } from 'app/match-detail/match-detail.module';
-
 const appRoutes: Routes = [
   {
     path: '',
