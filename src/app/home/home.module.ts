85a5aa7ab266452586ef77a59d89928c3908a21b
Dang Chi Son <kenshin@Shinigami.net.fpt>
2018-06-30 20:55:37 +0700

    Update style

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
+
 import { HeaderModule } from 'app/header/header.module';
 
 import { HomeComponent } from './home.component';
