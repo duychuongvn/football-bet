10d24981ff37b39170fa93b5fe7db25929736b05
Dang Chi Son <kenshin@Shinigami.net.fpt>
2018-07-01 16:32:30 +0700

    Edit header

--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { HeaderComponent } from './header.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule
+    RouterModule.forChild([])
   ],
   declarations: [
     HeaderComponent
