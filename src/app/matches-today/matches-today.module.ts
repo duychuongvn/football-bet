615e6aaf43b9f23c4d49d0ce983e26301cfc8f71
Dang Chi Son <kenshin@Shinigami-2.local>
2018-07-20 15:06:43 +0700

    Edit UI home page

--- src/app/matches-today/matches-today.module.ts
+++ src/app/matches-today/matches-today.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { AccordionModule } from 'ngx-bootstrap/accordion';
+
 import { MatchesTodayComponent } from './matches-today.component';
 
 const _route: Routes = [
@@ -13,6 +15,7 @@ const _route: Routes = [
 @NgModule({
   imports: [
     CommonModule,
+    AccordionModule.forRoot(),
     RouterModule.forChild(_route)
   ],
   declarations: [MatchesTodayComponent],
