bf3ea4df342a4e685f52e9f735e3382b8b87b27d
Dang Chi Son <kenshin@Shinigami-2.local>
2018-07-24 08:50:48 +0700

    Remove url admin

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -7,20 +7,12 @@ const appRoutes: Routes = [
     loadChildren: 'app/home/home.module#HomeModule'
   },
   {
-    path: 'admin',
-    loadChildren: 'app/admin/admin.module#AdminModule'
-  },
-  {
     path: 'user',
     loadChildren: 'app/user/user.module#UserModule'
   },
   {
     path: 'match-detail',
     loadChildren: 'app/match-detail/match-detail.module#MatchDetailModule'
-  },
-  {
-    path: 'coming-soon',
-    loadChildren: 'app/coming-soon/coming-soon.module#ComingSoonModule'
   },
   {
     path: 'how-it-works',
