b12a71136706e06daf94f1485888bb9f42ae5055
Dang Chi Son <kenshin@Shinigami.net.fpt>
2018-08-21 23:07:37 +0700

    Add environment

---
+++ src/environment/index.example.ts
@@ -0,0 +1,30 @@
+export default {
+  DOMAIN: process.env.DOMAIN,
+  APP_VERSION: '',
+  APP_OLD_VERSION: '',
+  APP_KEY: 'xxxxxxxxxxxxxxxxxxxx',
+  API: {
+    PATH: 'http://xxxxxxx',
+    PATH_IPFS: 'https://xxxxxxxxx',
+    EMULATE_JSON: false,
+    EMULATE_HTTP: true,
+    AUTHORIZATION: 'Authorization',
+    BEARER: 'Bearer '
+  },
+  LANGUAGE: {
+    ENGLISH: 'en',
+    VIETNAMESE: 'vi'
+  },
+  CURRENCY: {
+    ENGLISH: '$',
+    VIETNAMESE: 'VND'
+  },
+  NOTIFY: {
+    SUCCESS: 0,
+    ERROR: 1,
+    WARNING: 2,
+    INFO: 3,
+    DURATION: 2000
+  },
+  CONTRACT_ADDRESS: '0x0xxxx'
+}
