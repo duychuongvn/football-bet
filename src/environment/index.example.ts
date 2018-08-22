9457045ab08e331d5f37f0b237f750ef1bf670ad
Dang Chi Son <kenshin@Shinigami-2.local>
2018-08-22 15:30:29 +0700

    Update environment IPFS HASHID

--- src/environment/index.example.ts
+++ src/environment/index.example.ts
@@ -6,6 +6,7 @@ export default {
   API: {
     PATH: 'http://xxxxxxx',
     PATH_IPFS: 'https://xxxxxxxxx',
+    IPFS_HASHID: 'Qmaisz6NMhDB51cCvNWa1GMS7LU1pAxdF4Ld6Ft9kZEP2a',
     EMULATE_JSON: false,
     EMULATE_HTTP: true,
     AUTHORIZATION: 'Authorization',
