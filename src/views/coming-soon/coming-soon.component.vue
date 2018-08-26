c6844c300257bffbbbfd83860fd6376350d792fa
Dang Chi Son <kenshin@Shinigami-2.local>
2018-08-26 17:36:50 +0700

    Update coming-soon

--- src/views/coming-soon/coming-soon.component.vue
+++ src/views/coming-soon/coming-soon.component.vue
@@ -11,16 +11,22 @@
   })
   export default class ComingSoonPage extends Vue {
 
+    public deadlineTime = '2018/09/08 00:00:00';
+
     private socialIcons: Array<Object> = [
       {
         color: 'blue darken-3',
         name: 'fab fa-telegram-plane',
-        tooltip: 'telegram'
+        tooltip: 'Telegram',
+        link_social: 'https://t.me/joinchat/JmFIJw1TjIyq43r_3c-c1Q',
+        target: '_blank'
       },
       {
         color: 'light-blue darken-1',
         name: 'fab fa-twitter',
-        tooltip: 'Twitter'
+        tooltip: 'Twitter',
+        link_social: 'https://twitter.com/betbether',
+        target: '_blank'
       },
       {
         color: 'blue-grey darken-3',
@@ -31,9 +37,10 @@
         color: 'teal lighten-1',
         name: 'fas fa-envelope',
         tooltip: 'Support',
-        mailto: 'mailto:info@bether.bet'
+        link_social: 'mailto:info@bether.bet',
+        target: ''
       }
-    ]
+    ];
 
     mounted() {
       require('@/plugins/demo.js')
